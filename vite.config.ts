069a12925150f4994e72a17573107db555f633c8
HYI <3012177124@qq.com>
2024-03-01 09:23:36 +0800

    feat: vite-plugin-svg-icons

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import eslintPlugin from 'vite-plugin-eslint';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,14 +15,18 @@ export default defineConfig({
     Unocss(),
     AutoImport({
       imports: ['vue', { 'vue/macros': ['$ref'] }, 'vue-router'],
-      dirs: ['./src/composables', './src/store/modules'],
+      dirs: ['./src/hookss', './src/store/modules'],
       vueTemplate: true,
+      dts: true,
     }),
     Components({
       dts: true, // 对全局组件生成ts声明
       extensions: ['vue', 'tsx'],
       directoryAsNamespace: true, // 使用文件夹名为组件前缀
       resolvers: [], // ui库
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [`${path.resolve(__dirname, 'src/assets/svg')}/`],
     }),
   ],
   resolve: {
